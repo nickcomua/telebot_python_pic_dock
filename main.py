4cf1d526c4c826d17faf060d6d4bd3694f65fe0e
Nick <goodworcker@gmail.com>
2020-11-28 02:09:49 +0200

    v 1.3

--- main.py
+++ main.py
@@ -1,31 +1,26 @@
-import time
-import logging
-import threading
-import telebot
-import pickle
-import io
 import os.path
+import pickle
+import threading
+import time
+from os import path
 import random
-from telebot import types
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
-from apiclient import errors
-from googleapiclient.http import MediaIoBaseDownload, MediaFileUpload
+import io
+import telebot
+from googleapiclient.http import MediaIoBaseDownload
 import yaml
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from telebot import types
 
 
-def delete_file(service, file_id):
-    """Permanently delete a file, skipping the trash.
-
-  Args:
-    service: Drive API service instance.
-    file_id: ID of the file to delete.
-  """
-    try:
-        service.files().delete(fileId=file_id).execute()
-    except errors.HttpError as error:
-        print('An error occurred: %s' % error)
+def set(mar, num, x, y):
+    smile = smiles[x][y]
+    if num:
+        mar.keyboard[x][y].text = smile + " " + str(num)
+    else:
+        mar.keyboard[x][y].text = smile
+    return mar
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive']
@@ -55,138 +50,151 @@ results = service.files().list(q="'1u8CwWx_kErN5aZ-S-5L1Q6fP92tJR7XI' in parents
 items = results.get('files', [])
 while True:
     pageToken = results.get('nextPageToken', None)
-    print(pageToken)
     if not pageToken:
         break
-    print(items.__len__())
     results = service.files().list(q="'1u8CwWx_kErN5aZ-S-5L1Q6fP92tJR7XI' in parents",
                                    spaces='drive', pageToken=pageToken, pageSize=1000,
                                    fields="nextPageToken, files(id, name)"
                                    ).execute()
     items += results.get('files', [])
     pageToken = results.get('nextPageToken', None)
-print(items)
+print('indexed')
 a = {}
 ma = []
 token = "768083727:AAGifgRsDxB1ZY-4jkHPXd89Ag5fe_Zl87w"
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(token, num_threads=10)
 erex_data = {}
 love_data = {}
 cat_data = {}
+smiles = [['❤', '💔'], ['😄', '😳'], ['No 😸', '😸']]
 
-# TODO
-
-with open(r'cat_erek.yaml') as file:
-    cat_data = yaml.load(file, Loader=yaml.FullLoader)
-
-with open(r'data_erek.yaml') as file:
-    erex_data = yaml.load(file, Loader=yaml.FullLoader)
-
-with open(r'data_love.yaml') as file:
-    love_data = yaml.load(file, Loader=yaml.FullLoader)
-print(cat_data, erex_data, love_data)
+if path.exists(r'cat_erek.yaml'):
+    with open(r'cat_erek.yaml') as file:
+        cat_data = yaml.load(file, Loader=yaml.FullLoader)
+    with open(r'data_erek.yaml') as file:
+        erex_data = yaml.load(file, Loader=yaml.FullLoader)
+    with open(r'data_love.yaml') as file:
+        love_data = yaml.load(file, Loader=yaml.FullLoader)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_query(call):
-    print(call.data)
-    if call.data.split()[-1] == '0':
-        print(call)
-        if love_data.get(call.data.split()[0]):
-            for i in love_data.get(call.data.split()[0]):
-                if (i.data.split()[0] == call.data.split()[0]) & (i.from_user.id == call.from_user.id):
-                    print('deb')
-                    love_data[call.data.split()[0]].remove(i)
-            if love_data.get(call.data.split()[0]):
-                love_data[call.data.split()[0]] += [call]
+    drive_id = call.data.split()[0]
+    x = call.data.split()[-1]
+    if x == '0':
+        if love_data.get(drive_id):
+            for i in love_data.get(drive_id):
+                if (i.data.split()[0] == drive_id) & (i.from_user.id == call.from_user.id):
+                    love_data[drive_id].remove(i)
+
+            if love_data.get(drive_id):
+                love_data[drive_id] += [call]
             else:
-                love_data[call.data.split()[0]] = [call]
+                love_data[drive_id] = [call]
         else:
-            love_data[call.data.split()[0]] = [call]
-        print(love_data)
-        with open(r'data_love.yaml', 'w') as file:
-            documents = yaml.dump(love_data, file)
-        with open(r'data_love.yaml', 'r') as file:
-            bot.send_document('-447680338', file)
+            love_data[drive_id] = [call]
         bot.answer_callback_query(call.id)
-    elif call.data.split()[-1] == '1':
-        print(call)
-        if erex_data.get(call.data.split()[0]):
-            for i in erex_data.get(call.data.split()[0]):
-                if (i.data.split()[0] == call.data.split()[0]) & (i.from_user.id == call.from_user.id):
-                    print('deb')
-                    erex_data[call.data.split()[0]].remove(i)
-            if erex_data.get(call.data.split()[0]):
-                erex_data[call.data.split()[0]] += [call]
+    elif x == '1':
+        if erex_data.get(drive_id):
+            for i in erex_data.get(drive_id):
+                if (i.data.split()[0] == drive_id) & (i.from_user.id == call.from_user.id):
+                    erex_data[drive_id].remove(i)
+            if erex_data.get(drive_id):
+                erex_data[drive_id] += [call]
             else:
-                erex_data[call.data.split()[0]] = [call]
+                erex_data[drive_id] = [call]
         else:
-            erex_data[call.data.split()[0]] = [call]
-        print(erex_data)
-        with open(r'data_erek.yaml', 'w') as file:
-            documents = yaml.dump(erex_data, file)
-        with open(r'data_erek.yaml', 'r') as file:
-            bot.send_document('-447680338', file)
+            erex_data[drive_id] = [call]
+
         bot.answer_callback_query(call.id)
-    elif call.data.split()[-1] == '2':
-        print(call)
-        if cat_data.get(call.data.split()[0]):
-            for i in cat_data.get(call.data.split()[0]):
-                if (i.data.split()[0] == call.data.split()[0]) & (i.from_user.id == call.from_user.id):
-                    print('deb')
-                    cat_data[call.data.split()[0]].remove(i)
-            if cat_data.get(call.data.split()[0]):
-                cat_data[call.data.split()[0]] += [call]
+
+    elif x == '2':
+        if cat_data.get(drive_id):
+            for i in cat_data.get(drive_id):
+                if (i.data.split()[0] == drive_id) & (i.from_user.id == call.from_user.id):
+                    cat_data[drive_id].remove(i)
+
+            if cat_data.get(drive_id):
+                cat_data[drive_id] += [call]
             else:
-                cat_data[call.data.split()[0]] = [call]
+                cat_data[drive_id] = [call]
         else:
-            cat_data[call.data.split()[0]] = [call]
-        print(cat_data)
-        with open(r'cat_erek.yaml', 'w') as file:
-            documents = yaml.dump(cat_data, file)
-        with open(r'cat_erek.yaml', 'r') as file:
-            bot.send_document('-447680338', file)
+            cat_data[drive_id] = [call]
+
         bot.answer_callback_query(call.id)
+
+    c = {'0': 0, '1': 0}
+    for i in love_data.get(drive_id, []):
+        c[i.data.split()[-2]] += 1
+    mar = set(call.message.reply_markup, c['0'], 0, 0)
+    mar = set(mar, c['1'], 0, 1)
+    c = {'0': 0, '1': 0}
+    for i in erex_data.get(drive_id, []):
+        c[i.data.split()[-2]] += 1
+    mar = set(mar, c['0'], 1, 0)
+    mar = set(mar, c['1'], 1, 1)
+    c = {'0': 0, '1': 0}
+    for i in cat_data.get(drive_id, []):
+        c[i.data.split()[-2]] += 1
+    mar = set(mar, c['0'], 2, 0)
+    mar = set(mar, c['1'], 2, 1)
+    bot.edit_message_media(types.InputMediaPhoto(call.message.photo[0].file_id), chat_id=call.message.chat.id,
+                           message_id=call.message.id, reply_markup=mar)
 
 
 def fun1():
-    while True:
-        try:
-            bot.polling(none_stop=True, interval=0, timeout=0)
-        except:
-            time.sleep(10)
+    try:
+        bot.polling(none_stop=True, interval=0, timeout=0)
+    except:
+        time.sleep(10)
+        fun1()
 
 
+print('0')
 x = threading.Thread(target=fun1)
 x.start()
+
+print('1')
 while True:
     try:
-        while True:
-            for _ in range(10):
-                print("sth")
-                item = random.choice(items)
-                request = service.files().get_media(fileId=item['id'])
-                fh = io.FileIO('file.' + item['name'].split('.')[-1], 'wb')
-                downloader = MediaIoBaseDownload(fh, request)
-                done = False
-                while done is False:
-                    status, done = downloader.next_chunk()
-                    print("Download %d%%." % int(status.progress() * 100))
-                fh.close()
-                fh = io.FileIO('file.' + item['name'].split('.')[-1], 'rb')
-                # bot.send_message("-1001473134565", "123", disable_notification=True)
-                markup = types.InlineKeyboardMarkup()
-                markup.add(
-                    types.InlineKeyboardButton('❤️', callback_data=f"{item['id']} {item['name']} 1 0"),
-                    types.InlineKeyboardButton('💔', callback_data=f"{item['id']} {item['name']} 0 0"),
-                    types.InlineKeyboardButton('😄', callback_data=f"{item['id']} {item['name']} 1 1"),
-                    types.InlineKeyboardButton('😳', callback_data=f"{item['id']} {item['name']} 0 1"),
-                    types.InlineKeyboardButton('No 😸', callback_data=f"{item['id']} {item['name']} 0 2"),
-                    types.InlineKeyboardButton('😸', callback_data=f"{item['id']} {item['name']} 1 2"),
-                    row_width=2
-                )
-                bot.send_photo("-1001473134565", fh, reply_markup=markup)
-                fh.close()
-            time.sleep(60 * 60 * 2)
+        with open(r'data_love.yaml', 'w') as file:
+            documents = yaml.dump(love_data, file)
+        with open(r'data_erek.yaml', 'w') as file:
+            documents = yaml.dump(erex_data, file)
+        with open(r'cat_erek.yaml', 'w') as file:
+            documents = yaml.dump(cat_data, file)
+
+        with open(r'data_love.yaml', 'r') as file:
+            bot.send_document('-447680338', file)
+        with open(r'data_erek.yaml', 'r') as file:
+            bot.send_document('-447680338', file)
+        with open(r'cat_erek.yaml', 'r') as file:
+            bot.send_document('-447680338', file)
+        time.sleep(60)
+        for _ in range(10):
+            item = random.choice(items)
+            request = service.files().get_media(fileId=item['id'])
+            fh = io.FileIO('file.' + item['name'].split('.')[-1], 'wb')
+            downloader = MediaIoBaseDownload(fh, request)
+            done = False
+            while done is False:
+                status, done = downloader.next_chunk()
+                print("Download %d%%." % int(status.progress() * 100))
+            fh.close()
+            fh = io.FileIO('file.' + item['name'].split('.')[-1], 'rb')
+            # bot.send_message("-1001473134565", "123", disable_notification=True)
+            markup = types.InlineKeyboardMarkup()
+            markup.add(
+                types.InlineKeyboardButton('❤️', callback_data=f"{item['id']} {item['name']} 0 0"),
+                types.InlineKeyboardButton('💔', callback_data=f"{item['id']} {item['name']} 1 0"),
+                types.InlineKeyboardButton('😄', callback_data=f"{item['id']} {item['name']} 0 1"),
+                types.InlineKeyboardButton('😳', callback_data=f"{item['id']} {item['name']} 1 1"),
+                types.InlineKeyboardButton('No 😸', callback_data=f"{item['id']} {item['name']} 0 2"),
+                types.InlineKeyboardButton('😸', callback_data=f"{item['id']} {item['name']} 1 2"),
+                row_width=2
+            )
+            bot.send_photo("-1001473134565", fh, reply_markup=markup)
+            fh.close()
+        time.sleep(60 * 60 * 2)
     except:
         time.sleep(60 * 60 * 2)
